9d9ccccbfa1302885edeeb34f274e7bd8cd40b5b
Ben <xiaxuqiang@192.168.100.5>
2017-05-23 01:16:42 +0800

    first commit

---
+++ TestTemplateProject/Sources/ViewController/首页/HomePageVC.m
@@ -0,0 +1,27 @@
+//
+//  HomePageVC.m
+//  TestTemplateProject
+//
+//  Created by Ben on 2017/5/23.
+//  Copyright © 2017年 iOSStudio. All rights reserved.
+//
+
+#import "HomePageVC.h"
+
+@interface HomePageVC ()
+
+@end
+
+@implementation HomePageVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
