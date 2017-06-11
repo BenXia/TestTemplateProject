590b647e34d47f812dc2dd27a817125ba8d72e30
夏许强 <352229402@qq.com>
2017-06-11 19:37:05 +0800

    修改测试工程，方便支持多个子页面

---
+++ TestTemplateProject/Sources/ViewController/测试页面/TestViewController.m
@@ -0,0 +1,37 @@
+//
+//  TestViewController.m
+//  TestTemplateProject
+//
+//  Created by Ben on 2017/6/11.
+//  Copyright © 2017年 iOSStudio. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
