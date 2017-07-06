ffdafd134482b75c56033832eb6c30f68c8c63d6
夏许强 <xiaxuqiang@changingedu.com>
2017-07-06 18:45:26 +0800

    封装浮层相关为工具类 BNPopoverWindow

--- TestTemplateProject/Sources/Base/BasePopoverVC/BasePopoverVC.m
+++ TestTemplateProject/Sources/Base/BasePopoverVC/BasePopoverVC.m
@@ -1,30 +1,17 @@
 //
 //  BasePopoverVC.m
-//  TestTemplateProject
+//  QQingCommon
 //
 //  Created by Ben on 16/10/12.
 //  Copyright © 2016年 QQingiOSTeam. All rights reserved.
 //
 
 #import "BasePopoverVC.h"
-#import "BaseWindowRootVC.h"
-
-NSMutableArray *g_bottomPopupVCArray;
+#import "BNPopoverWindow.h"
 
 @interface BasePopoverVC ()
 
-@property (nonatomic, strong)   UIWindow *window;
-@property (nonatomic, strong)   UIWindow *originalKeyWindow;
-
-@property (nonatomic, strong)   UIViewController *rootViewController;
-@property (nonatomic, strong)   UIView *backgroundView;
-
-@property (nonatomic, weak)     NSLayoutConstraint *xConstraint;
-@property (nonatomic, weak)     NSLayoutConstraint *yConstraint;
-@property (nonatomic, weak)     NSLayoutConstraint *widthConstraint;
-
-@property (nonatomic, strong)   UIMotionEffectGroup *motionEffectGroup;
-@property (nonatomic, assign)   BOOL hasBeenDismissed;
+@property (nonatomic, weak) BNPopoverWindow *popWindow;
 
 @end
 
@@ -32,19 +19,9 @@ NSMutableArray *g_bottomPopupVCArray;
 
 #pragma mark - View life cycle
 
-+ (void)load {
-    if (!g_bottomPopupVCArray) {
-        g_bottomPopupVCArray = [NSMutableArray array];
-    }
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
-    self.view.translatesAutoresizingMaskIntoConstraints = NO;
-    
-    [self addMotionEffects];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -52,207 +29,73 @@ NSMutableArray *g_bottomPopupVCArray;
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - Properties
+#pragma mark - BNPopoverLayoutDelegate
 
-- (UIMotionEffectGroup *)motionEffectGroup {
-    if(!_motionEffectGroup) {
-        UIInterpolatingMotionEffect *verticalMotionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.y" type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
-        verticalMotionEffect.minimumRelativeValue = @(-10);
-        verticalMotionEffect.maximumRelativeValue = @(10);
-        
-        UIInterpolatingMotionEffect *horizontalMotionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.x" type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
-        horizontalMotionEffect.minimumRelativeValue = @(-10);
-        horizontalMotionEffect.maximumRelativeValue = @(10);
-        
-        _motionEffectGroup = [UIMotionEffectGroup new];
-        _motionEffectGroup.motionEffects = @[horizontalMotionEffect, verticalMotionEffect];
-    }
+- (PopoverLayoutType)popoverLayoutType {
+    return kPopoverLayoutType_Bottom;
+}
+
+- (CGSize)preferredContentSize {
+    return UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation) ? CGSizeMake([[UIScreen mainScreen] bounds].size.height, 200) : CGSizeMake([[UIScreen mainScreen] bounds].size.width, 300);
+}
+
+- (BOOL)backgroundPassThrough {
+    return NO;
+}
+
+- (BOOL)backgroundTapsDisabled {
+    return NO;
+}
+
+- (void)preShowLayoutForContentView:(UIView *)contentView rootVCView:(UIView *)rootVCView {
+    CGFloat heightToSet = [self preferredContentSize].height;
     
-    return _motionEffectGroup;
+    [contentView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(rootVCView.mas_centerX);
+        make.top.equalTo(rootVCView.mas_bottom);
+        make.width.equalTo(rootVCView.mas_width);
+        make.height.mas_equalTo(heightToSet);
+    }];
 }
 
-- (UIWindow *)window {
-    if (!_window) {
-        self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-        _window.windowLevel = UIWindowLevelStatusBar;
-        
-        BaseWindowRootVC *rootViewController = [BaseWindowRootVC createRootViewControllerWithStatusBarStyle:[UIApplication sharedApplication].statusBarStyle];
-        _window.rootViewController = rootViewController;
-    }
+- (void)normalLayoutForContentView:(UIView *)contentView rootVCView:(UIView *)rootVCView {
+    CGFloat heightToSet = [self preferredContentSize].height;
     
-    return _window;
+    [contentView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(rootVCView.mas_centerX);
+        make.bottom.equalTo(rootVCView.mas_bottom);
+        make.width.equalTo(rootVCView.mas_width);
+        make.height.mas_equalTo(heightToSet);
+    }];
 }
 
-- (UIView *)backgroundView {
-    if (!_backgroundView) {
-        self.backgroundView = [[UIView alloc] initWithFrame:CGRectZero];
-        _backgroundView.backgroundColor = RGBA(0, 0, 0, 0.5);
-        _backgroundView.translatesAutoresizingMaskIntoConstraints = NO;
-        
-        UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTappedOnBackgroundView:)];
-        [_backgroundView addGestureRecognizer:tapRecognizer];
-    }
+- (void)postDismissLayoutForContentView:(UIView *)contentView rootVCView:(UIView *)rootVCView {
+    CGFloat heightToSet = [self preferredContentSize].height;
     
-    return _backgroundView;
-}
-
-#pragma mark - Private methods
-
-- (void)addMotionEffects {
-    [self.view addMotionEffect:self.motionEffectGroup];
-}
-
-- (void)removeMotionEffects {
-    [self.view removeMotionEffect:self.motionEffectGroup];
-}
-
-- (void)addBottomPopoverVC:(BasePopoverVC *)bottomPopoverVC {
-    @synchronized (g_bottomPopupVCArray) {
-        [g_bottomPopupVCArray addObject:bottomPopoverVC];
-    }
-}
-
-- (void)removeBottomPopoverVC:(BasePopoverVC *)bottomPopoverVC {
-    @synchronized (g_bottomPopupVCArray) {
-        [g_bottomPopupVCArray removeObject:bottomPopoverVC];
-    }
-}
-
-#pragma mark - IBActions
-
-- (void)didTappedOnBackgroundView:(UIGestureRecognizer *)sender {
-    if(!self.backgroundTapsDisabled && !self.hasBeenDismissed) {
-        self.hasBeenDismissed = YES;
-        
-        [self dismissWithAnimated:YES];
-    }
+    [contentView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(rootVCView.mas_centerX);
+        make.top.equalTo(rootVCView.mas_bottom);
+        make.width.equalTo(rootVCView.mas_width);
+        make.height.mas_equalTo(heightToSet);
+    }];
 }
 
 #pragma mark - Public methods
 
-- (CGFloat)preferredContentHeight {
-    return UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation) ? 200 : 300;
-}
-
 - (void)showWithAnimated:(BOOL)animated {
-    self.rootViewController = self.window.rootViewController;
-    
-    {
-        self.originalKeyWindow = [UIApplication sharedApplication].keyWindow;
-        [self.window makeKeyAndVisible];
-        
-        // If we start in landscape mode also update the windows frame to be accurate
-        if([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0 && UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation)) {
-            self.window.frame = CGRectMake(0, 0, [[UIScreen mainScreen] bounds].size.height, [[UIScreen mainScreen] bounds].size.width);
-        }
+    if (!self.popWindow) {
+        self.popWindow = [BNPopoverWindow popoverWindowWithContentVC:self popoverDelegate:self];
     }
     
-    {
-        self.backgroundView.alpha = 0;
-        [self.rootViewController.view addSubview:self.backgroundView];
-        
-        [self.rootViewController.view addConstraint:[NSLayoutConstraint constraintWithItem:self.backgroundView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.rootViewController.view attribute:NSLayoutAttributeTop multiplier:1 constant:0]];
-        [self.rootViewController.view addConstraint:[NSLayoutConstraint constraintWithItem:self.backgroundView attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.rootViewController.view attribute:NSLayoutAttributeLeading multiplier:1 constant:0]];
-        [self.rootViewController.view addConstraint:[NSLayoutConstraint constraintWithItem:self.backgroundView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.rootViewController.view attribute:NSLayoutAttributeWidth multiplier:1 constant:0]];
-        [self.rootViewController.view addConstraint:[NSLayoutConstraint constraintWithItem:self.backgroundView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.rootViewController.view attribute:NSLayoutAttributeHeight multiplier:1 constant:0]];
-    }
-    
-    [self willMoveToParentViewController:self.rootViewController];
-    [self viewWillAppear:YES];
-    
-    [self.rootViewController addChildViewController:self];
-    [self.rootViewController.view addSubview:self.view];
-    
-    [self viewDidAppear:YES];
-    [self didMoveToParentViewController:self.rootViewController];
-    
-    self.xConstraint = [NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.rootViewController.view attribute:NSLayoutAttributeCenterX multiplier:1 constant:0];
-    self.yConstraint = [NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.rootViewController.view attribute:NSLayoutAttributeBottom multiplier:1 constant:0];
-    self.widthConstraint = [NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.rootViewController.view attribute:NSLayoutAttributeWidth multiplier:1 constant:0];
-    self.heightConstraint = [NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:0 constant:[self preferredContentHeight]];
-    
-    [self.rootViewController.view addConstraint:self.xConstraint];
-    [self.rootViewController.view addConstraint:self.yConstraint];
-    [self.rootViewController.view addConstraint:self.widthConstraint];
-    [self.view addConstraint:self.heightConstraint];
-    
-    [self.rootViewController.view setNeedsUpdateConstraints];
-    [self.rootViewController.view layoutIfNeeded];
-    
-    [self.rootViewController.view removeConstraint:self.yConstraint];
-    self.yConstraint = [NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.rootViewController.view attribute:NSLayoutAttributeBottom multiplier:1 constant:0];
-    [self.rootViewController.view addConstraint:self.yConstraint];
-    
-    [self.rootViewController.view setNeedsUpdateConstraints];
-    
-    if (animated) {
-        self.view.alpha = 0;
-        
-        //[UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:1 initialSpringVelocity:0.8 options:UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionAllowUserInteraction animations:^{
-        [UIView animateWithDuration:0.5 delay:0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
-            self.backgroundView.alpha = 1;
-            self.view.alpha = 1;
-            
-            [self.rootViewController.view layoutIfNeeded];
-        } completion:^(BOOL finished) {
-            [self addBottomPopoverVC:self];
-        }];
-    } else {
-        self.backgroundView.alpha = 1;
-        self.view.alpha = 1;
-        [self.rootViewController.view layoutIfNeeded];
-        
-        [self addBottomPopoverVC:self];
-    }
+    [self.popWindow showWithAnimated:animated];
 }
 
 - (void)dismissWithAnimated:(BOOL)animated {
-    if (animated) {
-        [self.rootViewController.view removeConstraint:self.yConstraint];
-        self.yConstraint = [NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.rootViewController.view attribute:NSLayoutAttributeBottom multiplier:1 constant:0];
-        [self.rootViewController.view addConstraint:self.yConstraint];
-        
-        [self.rootViewController.view setNeedsUpdateConstraints];
-        
-        [UIView animateWithDuration:0.5 delay:0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
-            self.backgroundView.alpha = 0;
-            self.view.alpha = 0;
-            
-            [self.rootViewController.view layoutIfNeeded];
-        } completion:^(BOOL finished) {
-            [self willMoveToParentViewController:nil];
-            [self viewWillDisappear:YES];
-            
-            [self.view removeFromSuperview];
-            [self removeFromParentViewController];
-            
-            [self didMoveToParentViewController:nil];
-            [self viewDidDisappear:YES];
-            
-            [self.backgroundView removeFromSuperview];
-            [self.window resignKeyWindow];
-            [self.originalKeyWindow makeKeyAndVisible];
-            self.window = nil;
-            [self removeBottomPopoverVC:self];
-            self.hasBeenDismissed = NO;
-        }];
-    } else {
-        [self willMoveToParentViewController:nil];
-        [self viewWillDisappear:YES];
-        
-        [self.view removeFromSuperview];
-        [self removeFromParentViewController];
-        
-        [self didMoveToParentViewController:nil];
-        [self viewDidDisappear:YES];
-        
-        [self.backgroundView removeFromSuperview];
-        [self.window resignKeyWindow];
-        [self.originalKeyWindow makeKeyAndVisible];
-        self.window = nil;
-        [self removeBottomPopoverVC:self];
-        self.hasBeenDismissed = NO;
+    if (!self.popWindow) {
+        return;
     }
+    
+    [self.popWindow dismissWithAnimated:animated];
 }
 
 @end
